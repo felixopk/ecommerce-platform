543816ef48ee30712cf529720fe709ebdb8cd798
Felix oppong <felixoppong2000@yahoo.com>
2026-01-04 14:13:16 +0100

    test: trigger new build

--- backend/src/products/products.controller.ts
+++ backend/src/products/products.controller.ts
@@ -7,7 +7,7 @@ export class ProductsController {
     return [
       { id: 1, name: 'T-shirt', price: 19.99 },
       { id: 2, name: 'Mug', price: 7.5 },
-      { id: 3, name: 'Cap', price: 14.99 },
+      { id: 3, name: 'Caps', price: 14.99 },
       { id:4, name: 'Men Shoes', price: 14.99 }
     ];
   }
