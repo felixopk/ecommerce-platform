9557c9fb0d131db16c0386329e456167a2d997ae
Felix oppong <felixoppong2000@yahoo.com>
2026-01-04 13:12:35 +0100

    updated products.controllers

--- backend/src/products/products.controller.ts
+++ backend/src/products/products.controller.ts
@@ -8,7 +8,7 @@ export class ProductsController {
       { id: 1, name: 'T-shirt', price: 19.99 },
       { id: 2, name: 'Mug', price: 7.5 },
       { id: 3, name: 'Cap', price: 14.99 }
-      { id: 3, name: 'Men Shoes', price: 14.99 }
+      { id:4, name: 'Men Shoes', price: 14.99 }
     ];
   }
 }
